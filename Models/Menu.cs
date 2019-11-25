b3ce7b50ad5a59e8ce0ad4946d85c668a4e2aac6
alumno informatica <alumno@ciis.usil.edu.pe>
2019-11-25 13:28:19 -0500

    pc2

---
+++ Models/Menu.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace pc4.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Entrada { get; set; }
+        public string Bebida { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
